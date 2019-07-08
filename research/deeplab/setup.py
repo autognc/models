8615f6c12f406d1a2b8070356ffa1777fd8dd4f2
Kevin Black <kevin3.black@gmail.com>
2019-07-07 22:38:53 -0400

    Add egg to setup.py

--- research/deeplab/setup.py
+++ research/deeplab/setup.py
@@ -12,5 +12,5 @@ setup(
         'pillow',
         'jupyter',
         'matplotlib',
-        'slim@git+https://github.com/autognc/models#subdirectory=research/slim'
+        'slim@git+https://github.com/autognc/models#egg=slim&subdirectory=research/slim'
     ])
